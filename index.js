57a7e6681d1bc9ad5a759286c6f540b151dc6d0b
Paul Jensen <paulbjensen@gmail.com>
2016-03-06 21:12:55 +0000

    Initial version

---
+++ index.js
@@ -0,0 +1,20 @@
+let hub;
+
+// This is an event emitter, used to handle communicating events
+// between different parts of the application.
+
+const riot = require('riot');
+
+function Hub () {
+  riot.observable(this);
+}
+
+
+function load () {
+  if (hub) return hub;
+  hub = new Hub();
+  return hub;
+}
+
+module.exports = load;
+
